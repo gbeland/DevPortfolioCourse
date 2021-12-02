0c1aaf5fbf1e608926605e9c7268a5ea53542192
graham-b <graham.b@samsung.com>
2021-12-02 14:47:40 -0700

    Working

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,20 @@
+using Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
+
+/*
+
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,3 +40,4 @@ namespace Client
         }
     }
 }
+*/
